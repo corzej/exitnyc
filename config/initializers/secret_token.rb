2d12de4563343313cbe131157dd05fbb0da46e07
corzej <corzej@gmail.com>
2013-08-26 15:46:36 -0400

    first commit with pull

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Exitnyc::Application.config.secret_key_base = 'a6c38d7a86bab5cd417c4a8892a8f444c31bcd77c9eb109224c1f9669699f144b01e250680e8bb29fe1f148e15429ce39c199db646e1318d09bcbc2479e523af'
