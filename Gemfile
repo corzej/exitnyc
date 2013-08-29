0e483e310e4a039a5054b775cdef0e11416436c0
corzej <corzej@gmail.com>
2013-08-29 17:49:18 -0400

    add arrow and connect ruby to js

--- Gemfile
+++ Gemfile
@@ -3,7 +3,9 @@ ruby '2.0.0'
 #ruby-gemset=railstutorial_rails_4_0
 
 gem 'rails', '4.0.0'
-
+gem 'gon'
+gem "smarter_csv"
+gem "json"
 group :development, :test do
   gem 'sqlite3', '1.3.7'
   gem 'rspec-rails', '2.13.1'
@@ -20,7 +22,7 @@ gem 'coffee-rails', '4.0.0'
 gem 'jquery-rails', '2.2.1'
 gem 'turbolinks', '1.1.1'
 gem 'jbuilder', '1.0.2'
-
+gem 'google_drive', '0.3.6'
 group :doc do
   gem 'sdoc', '0.3.20', require: false
 end
--- app/controllers/map_pages_controller.rb
+++ app/controllers/map_pages_controller.rb
@@ -1,4 +1,20 @@
 class MapPagesController < ApplicationController
   def home
-  end
+  	require "rubygems"
+  	require "google_drive"
+  	require "smarter_csv"
+  	require "json"
+
+  	session =GoogleDrive.login("corzej@gmail.com", "e10oegong") 
+  	@sheet = session.spreadsheet_by_title("ExitNYC").worksheets[0]
+  	#file name will be using number of sheet
+  	#gon.json = @sheet.rows
+  	arr =[]
+  	(2..1863).each do |n|
+  		data = {@sheet[1,1] => @sheet[n,1], @sheet[1,2] => @sheet[n,2], @sheet[1,3] => @sheet[n,3], @sheet[1,4] => @sheet[n,4]}
+  		data = data.to_json
+  		arr << JSON.load(data)
+  	end
+  	gon.json = arr
+ 	end
 end
